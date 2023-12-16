4ad0d9ef99dd2b498416145434e598ca142333f6
ufukyazici <ufukyazici.dev@gmail.com>
2023-12-16 19:53:28 +0300

    BLoC

--- lib/feature/travel/view/travel_view.dart
+++ lib/feature/travel/view/travel_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_state_management/product/padding/page_padding.dart';
 
 class TravelView extends StatefulWidget {
   const TravelView({super.key});
@@ -8,11 +9,32 @@ class TravelView extends StatefulWidget {
 }
 
 class _TravelViewState extends State<TravelView> {
+  final data = "See All";
+  final data2 = "Popular destinations near you";
+  final data3 = "Hey John! \n Where do you want to go today?";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("asdasdasd"),
+      appBar: AppBar(),
+      body: Padding(
+        padding: const PagePadding.all(),
+        child: Column(
+          children: [
+            Text(data3),
+            const TextField(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  data2,
+                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.bold),
+                ),
+                Text(data,
+                    style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Theme.of(context).colorScheme.error))
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
